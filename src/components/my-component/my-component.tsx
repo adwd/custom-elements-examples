f98059d9ec5010d50234c7db92b6140d3990988b
adwd <ecdt.mn@gmail.com>
2018-08-14 13:07:10 +0900

    first commit

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,23 @@
+import { Component, Prop, Event, EventEmitter } from '@stencil/core';
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true
+})
+export class MyComponent {
+
+  @Prop() name: string;
+  @Prop() repeat: number = 0;
+  @Event() ok: EventEmitter<{ message: string }>;
+
+  render() {
+    console.log(`render my-component, name: ${this.name}, repeat: ${this.repeat}`);
+    return (
+      <div>
+        <p>Hello, World! I'm {[...new Array(this.repeat || 1)].map(() => this.name).join(' ')}</p>
+        <button onClick={() => this.ok.emit({ message: 'ok' })}>ok</button>
+      </div>
+    );
+  }
+}
